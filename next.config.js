027bbffe5bc9caf87f65226784d8e429fc39d8e1
Tsour Lee adato <tsouri@MacBook-Pro-4.local>
2025-03-22 22:12:47 -0400

    fix: Configure for Netlify static export

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const path = require('path');
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['cdn.shopify.com', 'images.amazon.com', 'lh3.googleusercontent.com', 'images.unsplash.com'],
+    unoptimized: true,
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000', 'fascinating-biscochitos-5a6873.netlify.app'],
+    },
+  },
+  output: 'export',
+  distDir: 'out',
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
+};
+
+module.exports = nextConfig;
